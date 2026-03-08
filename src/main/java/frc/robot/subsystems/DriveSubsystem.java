a4acfd47fa1066631d35b294340c2b590e18771f
Addison King <kingaddiec@gmail.com>
2026-03-08 09:52:40 -0400

    Adjusting Robot speed

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -111,9 +111,9 @@ public class DriveSubsystem extends SubsystemBase {
    */
   public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
     // Convert the commanded speeds into the correct units for the drivetrain
-    double xSpeedDelivered = xSpeed * DriveConstants.kMaxSpeedMetersPerSecond;
-    double ySpeedDelivered = ySpeed * DriveConstants.kMaxSpeedMetersPerSecond;
-    double rotDelivered = rot * DriveConstants.kMaxAngularSpeed;
+    double xSpeedDelivered = (xSpeed * DriveConstants.kMaxSpeedMetersPerSecond) / 1.5;
+    double ySpeedDelivered = (ySpeed * DriveConstants.kMaxSpeedMetersPerSecond) / 1.5;
+    double rotDelivered = (rot * DriveConstants.kMaxAngularSpeed) / 1.5;
 
     var swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
         fieldRelative
